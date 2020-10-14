dcd9fd15e87e51d6caaa7216093891542c90f89e
JOHN CARLOS PAGADUAN (GEDI-ISD-OOCLL/MNL) <PAGADJO@OOCL.COM>
2020-10-14 20:58:23 +0800

    [Practice 7][John Carlos]:Feature - added attributes and methods in Student.java"

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,21 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    Klass klass = new Klass();
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Klass getKlass(){
+        return klass;
+    }
+    public int getKlassNum(){return klass.getNumber();}
+    public String introduce(){
+        return super.introduce() +" "+  "I am a Student. I am at " + klass.getDisplayName() + ".";
+    }
 }
