d1a79f900e1eb8e013906ea629f42b783304d7bb
JOHN CARLOS PAGADUAN (GEDI-ISD-OOCLL/MNL) <PAGADJO@OOCL.COM>
2020-10-14 22:44:25 +0800

    [Practice 8][John Carlos]:Feature - added getLeader and assignLeader methods in Klass.java

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -2,21 +2,26 @@ package practice08;
 
 public class Klass {
     Integer number;
+    Student klassLeader;
 
     public Klass(Integer number) {
         this.number = number;
     }
-
     public Klass() {
 
     }
-
     public Integer getNumber() {
         return number;
     }
-
     public String getDisplayName() {
         return "Class " + number;
     }
+    public Student getLeader() {
+        return klassLeader;
     }
-
+    public void assignLeader(Student studentLeader) {
+        if(this.number == studentLeader.getKlass().getNumber()) {
+            this.klassLeader = studentLeader;
+        }
+    }
+}
