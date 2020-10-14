ee6209856db49655c874fb727353c2823ddd893a
JOHN CARLOS PAGADUAN (GEDI-ISD-OOCLL/MNL) <PAGADJO@OOCL.COM>
2020-10-14 22:59:22 +0800

    [Practice 9][John Carlos]:Feature - added methods in Student.java

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,26 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+    Klass klass = new Klass();
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public Klass getKlass(){
+        return klass;
+    }
+
+    public int getKlassNum(){return klass.getNumber();}
+
+    public String introduce(){
+        String ifLeader = " I am a Student. I am Leader of ";
+        String ifNotLeader = " I am a Student. I am at ";
+        return  this.klass.getLeader() != null ? super.introduce() + ifLeader + klass.getDisplayName() + "." : super.introduce() + ifNotLeader + klass.getDisplayName() + ".";
+    }
 }
+
