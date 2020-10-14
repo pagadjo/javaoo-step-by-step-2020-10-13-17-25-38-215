ffc171cd51aa06e23b39f17df38a63565f5fbc70
JOHN CARLOS PAGADUAN (GEDI-ISD-OOCLL/MNL) <PAGADJO@OOCL.COM>
2020-10-14 18:03:43 +0800

    [Practice 6][John Carlos]:Feature - added attributes, methods and additional constructor to accommodate different parameters in Teacher.java; Removed comments in Practice06Test.java

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,23 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass(){return klass;}
+    public String introduce(){
+        String mainString = super.introduce() +" "+  "I am a Teacher. ";
+        String hasClass =  "I teach Class " + getKlass() + ".";
+        String noClass = "I teach No Class.";
+
+        return klass !=0 ? mainString + hasClass :mainString + noClass;
+    }
 }
