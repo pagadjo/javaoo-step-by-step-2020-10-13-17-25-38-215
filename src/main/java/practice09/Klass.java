e1bd4c4646ae1ef5d70f662659a54e5adada2a0d
JOHN CARLOS PAGADUAN (GEDI-ISD-OOCLL/MNL) <PAGADJO@OOCL.COM>
2020-10-14 22:57:53 +0800

    [Practice 9][John Carlos]:Feature - added methods in Klass.java

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,30 @@
 package practice09;
 
 public class Klass {
+    Integer number;
+    Student klassLeader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+    public Klass() {}
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public Student getLeader() {
+        return klassLeader;
+    }
+
+    public void assignLeader(Student studentLeader) {
+        if(this.number == studentLeader.getKlass().getNumber()) {
+            this.klassLeader = studentLeader;
+        }else{ System.out.print("It is not one of us.\n");}
+    }
+
 }
