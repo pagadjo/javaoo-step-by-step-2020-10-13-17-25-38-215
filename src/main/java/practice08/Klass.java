0cb3bec2eb6c574507476ae1b6237d7381e8b9c8
JOHN CARLOS PAGADUAN (GEDI-ISD-OOCLL/MNL) <PAGADJO@OOCL.COM>
2020-10-14 22:07:35 +0800

    [Practice 8][John Carlos]:Feature - added attributes, methods in Klass.java without getLeader and assignLeader methods

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,22 @@
 package practice08;
 
 public class Klass {
-}
+    Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Klass() {
+
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+    }
+
