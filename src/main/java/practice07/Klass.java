cb23cbaabd4bcd5dc40daa41e54fe077595d5996
JOHN CARLOS PAGADUAN (GEDI-ISD-OOCLL/MNL) <PAGADJO@OOCL.COM>
2020-10-14 20:56:54 +0800

    [Practice 7][John Carlos]:Feature - added attributes and methods in Klass.java"

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,22 @@
 package practice07;
 
 public class Klass {
+    Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Klass() {
+
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
 }
