042af2b7fa2cf77aa09ec2ffc989b0a4f33e9cd0
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 13:15:10 +0800

    [Practice 10][John Carlos]Feature: added methods in Klass.java

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,37 @@
 package practice10;
 
 public class Klass {
+    private Integer number;
+    private Student klassLeader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Klass() {
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public Student getLeader() {
+        return klassLeader;
+    }
+
+    public void assignLeader(Student studentLeader) {
+        if (this.number.equals(studentLeader.getKlass().getNumber())) {
+            this.klassLeader = studentLeader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
 }
