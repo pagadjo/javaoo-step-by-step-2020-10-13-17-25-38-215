db4eaa21eec5f8b1d382eb8605792516d6e4eee5
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 13:18:46 +0800

    [Practice 10][John Carlos]Feature: added methods in Student.java

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,33 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    Klass klass = new Klass();
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public int getKlassNum() {
+        return klass.getNumber();
+    }
+
+    public String introduce() {
+        String mainString = super.introduce();
+        String ifLeader = " I am a Student. I am Leader of " + klass.getDisplayName() + ".";
+        String ifNotLeader = " I am a Student. I am at " + klass.getDisplayName() + ".";
+        return this.klass.getLeader() != null ? mainString + ifLeader : mainString + ifNotLeader;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
