038edfde7fb5c44ffd4d486a4d4dc5a95bcf93ee
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 13:16:00 +0800

    [Practice 10][John Carlos]Feature: added methods in Person.java

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,45 @@
 package practice10;
 
+import java.util.Objects;
+
 public class Person {
+    Klass klass = new Klass();
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String introduce() {
+        return "My name is " + getName() + ". I am " + getAge() + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
